5cd24b28ab3ea648b29c39e2d860bd4d4b2f3840
emypye37 <40176364+emypye37@users.noreply.github.com>
2022-09-23 03:22:46 +0100

    Add files via upload

---
+++ grolldle3.js
@@ -0,0 +1,241 @@
+import { WORDS } from "./words.js";
+
+const NUMBER_OF_GUESSES = 6;
+let guessesRemaining = NUMBER_OF_GUESSES;
+let currentGuess = [];
+let nextLetter = 0;
+// let rightGuessString = WORDS[Math.floor(Math.random() * 4)];
+let rightGuessString = "float";
+
+function initBoard() {
+  let board = document.getElementById("game-board");
+
+  for (let i = 0; i < NUMBER_OF_GUESSES; i++) {
+    let row = document.createElement("div");
+    row.className = "letter-row";
+
+    for (let j = 0; j < 5; j++) {
+      let box = document.createElement("div");
+      box.className = "letter-box";
+      row.appendChild(box);
+    }
+
+    board.appendChild(row);
+  }
+}
+
+function insertLetter(pressedKey) {
+  if (nextLetter === 5) {
+    return;
+  }
+  pressedKey = pressedKey.toLowerCase();
+
+  let row = document.getElementsByClassName("letter-row")[6 - guessesRemaining];
+  let box = row.children[nextLetter];
+  animateCSS(box, "pulse");
+  box.textContent = pressedKey;
+  box.classList.add("filled-box");
+  currentGuess.push(pressedKey);
+  nextLetter += 1;
+}
+
+function deleteLetter() {
+  let row = document.getElementsByClassName("letter-row")[6 - guessesRemaining];
+  let box = row.children[nextLetter - 1];
+  box.textContent = "";
+  box.classList.remove("filled-box");
+  currentGuess.pop();
+  nextLetter -= 1;
+}
+
+function checkGuess() {
+  let row = document.getElementsByClassName("letter-row")[6 - guessesRemaining];
+  let guessString = "";
+  let rightGuess = Array.from(rightGuessString);
+
+  for (const val of currentGuess) {
+    guessString += val;
+  }
+
+  if (guessString.length != 5) {
+    toastr.error("Not enough letters!");
+    return;
+  }
+
+  if (!WORDS.includes(guessString)) {
+    toastr.error("Word not in list!");
+    return;
+  }
+
+  for (let i = 0; i < 5; i++) {
+    let letterColor = "";
+    let box = row.children[i];
+    let letter = currentGuess[i];
+
+    let letterPosition = rightGuess.indexOf(currentGuess[i]);
+    // is letter in the correct guess
+    if (letterPosition === -1) {
+      letterColor = "#3a3a3c";
+    } else {
+      // now, letter is definitely in word
+      // if letter index and right guess index are the same
+      // letter is in the right position
+      if (currentGuess[i] === rightGuess[i]) {
+        // shade green
+        letterColor = "#538c4f";
+      } else {
+        // shade box yellow
+        letterColor = "#b69f3c";
+      }
+
+      rightGuess[letterPosition] = "#";
+    }
+
+    let delay = 250 * i;
+    setTimeout(() => {
+      //flip box
+      animateCSS(box, "flipInX");
+      //shade box
+      box.style.backgroundColor = letterColor;
+      box.style.borderColor = letterColor;
+      shadeKeyBoard(letter, letterColor);
+    }, delay);
+  }
+
+  if (guessString === rightGuessString) {
+    toastr.options = {
+      closeButton: true,
+      debug: false,
+      newestOnTop: false,
+      progressBar: false,
+      positionClass: "toast-bottom-center",
+      preventDuplicates: false,
+      onclick: null,
+      showDuration: "300",
+      hideDuration: "1000",
+      timeOut: 0,
+      extendedTimeOut: 0,
+      showEasing: "swing",
+      hideEasing: "linear",
+      showMethod: "fadeIn",
+      hideMethod: "fadeOut",
+      tapToDismiss: true,
+    };
+
+    toastr.success(
+      `I must be psychic cause I knew we'd meet like this again. Too bad for you, though, cause I heard a rumor that there's a link to boobies in the "wrong answer" message of this Grolldle. But <a target="_blank" href="https://twinery.org/2/#/stories/ad757c9e-6e9a-498e-abeb-7c4906552bfc/test/c082bc33-1706-4006-9bb3-26ab688c6574">here's</a> something else pretty cool to make it up to you xoxo`
+    );
+    guessesRemaining = 0;
+    return;
+  } else {
+    guessesRemaining -= 1;
+    currentGuess = [];
+    nextLetter = 0;
+
+    if (guessesRemaining === 0) {
+      toastr.options = {
+        closeButton: true,
+        debug: false,
+        newestOnTop: false,
+        progressBar: false,
+        positionClass: "toast-bottom-center",
+        preventDuplicates: false,
+        onclick: null,
+        showDuration: "300",
+        hideDuration: "1000",
+        timeOut: 0,
+        extendedTimeOut: 0,
+        showEasing: "swing",
+        hideEasing: "linear",
+        showMethod: "fadeIn",
+        hideMethod: "fadeOut",
+        tapToDismiss: true,
+      };
+      toastr.error(
+        `Are these wrong answer messages a complete waste of time? I hope not. If you're reading this, you either guessed wrong on purpose just to get here, or I've actually got you on one. I'd send you on a wild link chase, but I'm bad at keeping secrets so let's do more <a target="_blank" href="https://twinery.org/2/#/stories/ad757c9e-6e9a-498e-abeb-7c4906552bfc/test/3aa1a755-ea27-4708-98c0-6ee82a09cd0a">birthday</a> things :) `
+      );
+      // toastr.error("You've run out of guesses! Game over!");
+      toastr.info(`The right word was: "${rightGuessString}"`);
+    }
+  }
+}
+
+function shadeKeyBoard(letter, color) {
+  for (const elem of document.getElementsByClassName("keyboard-button")) {
+    if (elem.textContent === letter) {
+      let oldColor = elem.style.backgroundColor;
+      if (oldColor === "#538c4f") {
+        return;
+      }
+
+      if (oldColor === "#b69f3c" && color !== "#538c4f") {
+        return;
+      }
+
+      elem.style.backgroundColor = color;
+      elem.style.borderColor = color;
+      break;
+    }
+  }
+}
+
+initBoard();
+
+document.addEventListener("keyup", (e) => {
+  if (guessesRemaining === 0) {
+    return;
+  }
+
+  let pressedKey = String(e.key);
+  if (pressedKey === "Backspace" && nextLetter !== 0) {
+    deleteLetter();
+    return;
+  }
+
+  if (pressedKey === "Enter") {
+    checkGuess();
+    return;
+  }
+
+  let found = pressedKey.match(/[a-z]/gi);
+  if (!found || found.length > 1) {
+    return;
+  } else {
+    insertLetter(pressedKey);
+  }
+});
+
+document.getElementById("keyboard-cont").addEventListener("click", (e) => {
+  const target = e.target;
+
+  if (!target.classList.contains("keyboard-button")) {
+    return;
+  }
+  let key = target.textContent;
+
+  if (key === "Del") {
+    key = "Backspace";
+  }
+
+  document.dispatchEvent(new KeyboardEvent("keyup", { key: key }));
+});
+
+const animateCSS = (element, animation, prefix = "animate__") =>
+  // We create a Promise and return it
+  new Promise((resolve, reject) => {
+    const animationName = `${prefix}${animation}`;
+    // const node = document.querySelector(element);
+    const node = element;
+    node.style.setProperty("--animate-duration", "0.3s");
+
+    node.classList.add(`${prefix}animated`, animationName);
+
+    // When the animation ends, we clean the classes and resolve the Promise
+    function handleAnimationEnd(event) {
+      event.stopPropagation();
+      node.classList.remove(`${prefix}animated`, animationName);
+      resolve("Animation ended");
+    }
+
+    node.addEventListener("animationend", handleAnimationEnd, { once: true });
+  });
